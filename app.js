51ac191a9d4a364eb7cfef363f6d5b158e40aa42
usaidahmed01 <usaid423@gmail.com>
2023-10-23 03:53:45 -0700

    JS assignment

---
+++ app.js
@@ -0,0 +1,57 @@
+
+// Alerts: 01
+
+// alert('Error! Please enter a valid password')
+// alert('Welcome to JS Land...'+'\n'+'HappyCoding!')
+// alert('Welcome to JS Land...')
+// alert('Happy Coding')
+
+
+
+// Var For Strings: 02
+
+//  const name = 'Usaid Ahmed'
+//  const age =  '18 Years Old'
+//  const uni =  'NED'
+//  const mail = 'My Email address is usaid423@gmail.com'
+//  const book = 'I am trying to learn from the book "A smarter wqay to learn JS" '
+
+//  alert(name)
+// alert(age)
+// alert(uni)
+// alert(mail)
+// alert(book)
+
+
+
+// Var For Numbers: 03
+
+// const num = Number('My birth year is 1990')
+// alert(typeof(num))
+// console.log(typeof(num))
+
+
+
+// Maths Expression: 04
+
+// const name = prompt('Enter Your Name')
+// const chemMarks = +prompt('Enter Your Chemistry Marks')
+// const mathsMarks = +prompt('Enter Your Maths Marks')
+// const phyMarks = +prompt('Enter Your Physics Marks')
+// const urduMarks = +prompt('Enter Your Urdu Marks')
+// const engMarks = +prompt('Enter Your English Marks')
+// const pstMarks = +prompt('Enter Your Pak Studies Marks')
+// const obtMarks = chemMarks + mathsMarks + phyMarks + urduMarks + engMarks + pstMarks 
+
+// const totalMarks = 550
+// const percentage = obtMarks / totalMarks * 100
+
+// document.write(name +  ' achieved ' + percentage +  ' %.')
+// console.log(name + ' achieved ' + percentage + ' %.')
+
+
+
+
+
+
+
